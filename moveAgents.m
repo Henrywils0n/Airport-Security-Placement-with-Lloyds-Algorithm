7d531305b02a308c910c15c2d43034ab35ca7712
aidanpawliw <94487939+aidanpawliw@users.noreply.github.com>
2021-11-28 13:56:50 -0500

    Add files via upload

---
+++ moveAgents.m
@@ -0,0 +1,55 @@
+function [agentPositions, distanceTravelled, energy] = moveAgents(agentPositions,...
+    centroids, sides, dt, energy, velocityType, maxVelocity, scaleFactor)
+
+orientation = centroids - agentPositions;
+magOrientation = vecnorm(orientation,2,2);
+orientation = bsxfun(@rdivide, orientation, magOrientation(:));
+orientation(isnan(orientation)) = 0;
+
+if strcmp(velocityType, 'Constant Velocity')
+    velocity = maxVelocity;
+else
+    velocity = magnitude*scaleFactor;
+    velocity(velocity>maxVelocity) = maxVelocity;
+    velocity(isnan(velocity)) = 0;
+end
+deltaPosition = velocityFunction(orientation, velocity, dt);
+deltaPosition(agentPositions+deltaPositions>sides) = 0;
+deltaPosition(agentPositions+deltaPositions<0) = 0;
+agentPositions = agentPositions + deltaPosition;
+deltaEnergy = energyfunction(velocity, deltaPosition, dt);
+energy = energy + deltaEnergy;
+distanceTravelled = sum(vecnorm(deltaPosition,2,2));
+
+%% moveAgents
+% Moves each agent towards its assigned centroid. Must ensure agents don't
+% move out of bounds.
+% 
+% Parameters:
+%   agentPositions
+%     n-by-2 vector of the x, y positions for n agents before moving
+%   centroids
+%     n-by-2 vector of the x, y centroid locations that agents move towards
+%   dt
+%     Simulated time step
+%   sides
+%     Side length of the arena
+%   energy
+%     n-by-1 vector of agent energy before moving
+%   velocityType
+%     Either "Constant Velocity" or "Proportional Velocity" as determined
+%     in the GUI
+%   maxVelocity
+%     If velocityType = "Constant Velocity", this is the constant velocity
+%     Otherwise, this is the maximum velocity at which agents can travel
+%   scaleFactor
+%     Used for "Proportional Velocity". Velocity is proportional to 
+%     distance between agent and centroid, scaled by the scale factor
+%
+% Returns:
+%   agentPositions
+%     n-by-2 vector of x, y positions of for the n agents after moving
+%   distanceTravelled
+%     n-by-1 vector of distance travelled by each agent this iteration
+%   energy
+%     n-by-1 vector of agent energy after moving
